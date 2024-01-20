82c58fba576117ab9d949e8d6597048bf73dca1f
MaheshKafle <mahesh.kafle@lisec.com>
2024-01-20 18:59:25 +0400

    Aliasing and cy.log usage

--- cypress/e2e/Test1.cy.js
+++ cypress/e2e/Test1.cy.js
@@ -6,22 +6,32 @@ describe('My First Test Suite',
 {
     it('My First Testcase', ()=>
     {   
-        cy.viewport(1280,800)
         cy.visit('https://rahulshettyacademy.com/seleniumPractise/#/')
+        cy.viewport(1280,800)
         cy.get('.search-keyword').type('ca')
         cy.wait(1000)
         //verify four products gets listed after searching with keyword 'ca'
         cy.get('.product:visible').should('have.length', 4) //way to select only visible elements in jquery by using :visible keyword
         //Parent child chaining to add product to the cart
-        cy.get('.products').find('.product').eq(1).contains('ADD TO CART').click()   
+        cy.get('.products').as('productLocator') //aliasing concept
+        cy.get('@productLocator').find('.product').eq(1).contains('ADD TO CART').click()   
         //Using text() method to click on products
-        cy.get('.products').find('.product').each(($ele, index, $list)=>
+        cy.get('@productLocator').find('.product').each(($ele, index, $list)=>
         {
             const text = $ele.find('.product-name').text()
             if(text.includes('Cashews'))
             {
               cy.wrap($ele).find('button').click()  //we need to wrap the ele to resolve the promise here
             }
+        })
+        //assert if logo text is correctly displayed
+        cy.get('.brand').should('have.text','GREENKART')
+ 
+        //this is to print in logs
+        cy.get('.brand').then((logoelement)=>
+        {
+            cy.log(logoelement.text())
+        
         })
     })
 })  
