6e415d11ab129e4df26aecb886bde9eb4ae71bf6
MaheshKafle <mahesh.kafle@lisec.com>
2024-03-12 15:00:19 +0400

    fixtures, validating attribute properties and their behavior in cypress

--- cypress.config.js
+++ cypress.config.js
@@ -2,6 +2,9 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
+    viewportWidth: 1366,
+    viewportHeight: 768,
+
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
